b6bc98a4be7979a67b59faa8c98e0531cfb8a105
Higor Augusto da Silva Santos <higor.santos@unimedvr.com.br>
2017-12-01 18:01:59 -0200

    atualização

--- src/SalaoMabhiBundle/Controller/PrincipalController.php
+++ src/SalaoMabhiBundle/Controller/PrincipalController.php
@@ -9,10 +9,42 @@ class PrincipalController extends Controller
 {
 
     /**
-     * @Route("/")
+     * @Route("/", name="home")
      */
     public function indexAction()
     {
         return $this->render('SalaoMabhiBundle:Principal:principal.html.twig');
+    }
+
+    /**
+     * @Route("/agenda", name="agenda")
+     */
+    public function carregar_agendaAction()
+    {
+        return $this->render('SalaoMabhiBundle:Agenda:agenda.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function carregar_loginAction()
+    {
+        return $this->render('SalaoMabhiBundle:Login:login.html.twig');
+    }
+
+    /**
+     * @Route("/menu_principal", name="menu_principal")
+     */
+    public function carregar_menu_principalAction()
+    {
+        return $this->render('SalaoMabhiBundle:Menu_Principal:menu_principal.html.twig');
+    }
+
+    /**
+     * @Route("/menu_principal/cadastro", name="cadastro")
+     */
+    public function carregar_contatosAction()
+    {
+        return $this->render('SalaoMabhiBundle:Contatos:cadastro.html.twig');
     }
 }
--- web/js/b1739c1.js
+++ web/js/b1739c1.js
@@ -1,3 +1,3 @@
 window.onload = function(){
-    console.log('Onload ahahha');
+    console.log('Onload OOAOAOAOAOAOAO');
 }
